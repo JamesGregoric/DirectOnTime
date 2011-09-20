79b803c011b4a64dec05cb1cfe0b3de28707187d
Shibu Raj <shibu.raj@gmail.com>
2011-09-20 11:03:51 -0500

    Added the base project files

---
+++ src/DirectOnTime.Infrastructure/Program.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) Direct General Corporation. All rights reserved.
+//  
+// Reproduction or transmission in whole or in part, in any form or 
+// by any means, electronic, mechanical or otherwise, is prohibited 
+// without the prior written consent of the copyright owner.
+//  
+// SolutionName : DirectOnTime
+// Project/Filename: DirectOnTime.Infrastructure / Program.cs
+// Created By (Date): Shibu K. Raj - {pgskr} (20/09/2011 9:59 AM)
+// Last Modified By (Date) : Shibu K. Raj - {pgskr} (20/09/2011 9:59 AM)
+
+namespace DirectOnTime.Infrastructure
+{
+    public static class Program
+    {
+        private static  void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ src/DirectOnTime.PaymentProcessor/Program.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) Direct General Corporation. All rights reserved.
+//  
+// Reproduction or transmission in whole or in part, in any form or 
+// by any means, electronic, mechanical or otherwise, is prohibited 
+// without the prior written consent of the copyright owner.
+//  
+// SolutionName : DirectOnTime
+// Project/Filename: DirectOnTime.PaymentProcessor / Program.cs
+// Created By (Date): Shibu K. Raj - {pgskr} (20/09/2011 10:00 AM)
+// Last Modified By (Date) : Shibu K. Raj - {pgskr} (20/09/2011 10:00 AM)
+
+namespace DirectOnTime.PaymentProcessor
+{
+    public static class Program {
+        private static void Main(string[] args) {
+        }
+    }
+}
